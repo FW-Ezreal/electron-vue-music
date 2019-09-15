02b697f2f4c0baa33fd66a5b9b12b9102fe58824
xinwei.liu <xinwei.liu@kuwo.cn>
2019-09-15 12:51:52 +0800

    feat: install electron-vue

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'electron-music-box'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
